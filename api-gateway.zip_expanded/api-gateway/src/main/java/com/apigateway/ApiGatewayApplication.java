71b21d9461cf54d30f3b1c33bd24c3452184f3ce
HailemichaelHagos <Hailemichaelteklu@gmail.com>
2022-05-30 15:09:40 -0500

    added client

--- api-gateway.zip_expanded/api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
+++ api-gateway.zip_expanded/api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient  
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
